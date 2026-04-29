188f8e5426637d69dbf1aa7ee6818105c67c3a4b
Richard DJE <richard.dje@gmail.com>
2026-04-29 13:28:28 +0200

    SpecEntry: inline Data::Dumper loading
    - remove the separate SpecEntry Data::Dumper loader wrapper
    - lazy-load Data::Dumper directly inside the dump helper
    - lock the slimmer source shape in phase0 and refresh architecture docs

--- perl/LinkedSpec/SpecEntry.pm
+++ perl/LinkedSpec/SpecEntry.pm
@@ -61,11 +61,6 @@ sub _require_trace_pkg {
  return 1
 }
 
-sub _require_data_dumper_pkg {
- LinkedSpec::OwnerDispatch::require_pkg(__PACKAGE__, 'Data::Dumper');
- return 1
-}
-
 sub _trace_enter {
  my @args = @_;
  return LinkedSpec::OwnerDispatch::call_preserving_err(sub {
@@ -109,7 +104,7 @@ sub _trace_should_dump {
 sub _dump_value {
  my ($value) = @_;
  return LinkedSpec::OwnerDispatch::call_preserving_err(sub {
-  _require_data_dumper_pkg();
+  LinkedSpec::OwnerDispatch::require_pkg(__PACKAGE__, 'Data::Dumper');
   return Data::Dumper::Dumper($value)
  })
 }
